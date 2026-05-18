076670e9f1c24a16446f4324e76cb5d264b2ab32
Sami <maruf.the.dev@gmail.com>
2026-05-19 02:57:00 +0600

    update the config file for cdn services

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**', 
+      },
+      {
+        protocol: 'https',
+        hostname: '"images.unsplash.com"',
+        port: '',
+        pathname: '/**', 
+      },
+
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
