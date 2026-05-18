4f8db906102d1346b209242776fb198e64ba08ce
Sami <maruf.the.dev@gmail.com>
2026-05-19 04:02:00 +0600

    fixing db connect function

--- src/lib/db.js
+++ src/lib/db.js
@@ -17,8 +17,6 @@ export async function connectDB(){
     if(!cached.promise){
         const opts = {
             bufferCommands: false,
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
             dbName: 'art-work',   
             tlsAllowInvalidCertificates: true     
         };
