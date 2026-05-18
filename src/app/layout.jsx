5a9049328f81bb5f77a9cb5dff3a04aa3ca96241
sh6481511-ui <sh6481511@gmail.com>
2026-05-19 00:10:35 +0600

    layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,1 +1,25 @@
-e
+import Navbar from '@/src/components/Navbar'; 
+import './globals.css'; 
+
+export const metadata = {
+  title: 'Virtual Art Gallery',
+  description: 'Immersive digital space for classical and digital masterpieces.',
+};
+
+export default function RootLayout({ children }) {
+
+  const currentUser = {
+    isLoggedIn: false, 
+    name: 'Alex'
+  };
+
+  return (
+    <html lang="en">
+      <body>
+        <Navbar user={currentUser} />
+        
+        {children}
+      </body>
+    </html>
+  );
+}
